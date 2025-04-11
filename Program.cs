06187145aec3a42581cea0aa469f13113faad54f
Alexier Czekiel Gallentes <alexier.czekiel.gallentes@student.ateneo.edu>
2025-04-11 12:03:38 +0800

    y

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Promedics
@@ -10,6 +11,86 @@ namespace Promedics
     {
         static void Main(string[] args)
         {
+            // Super Basic Version 
+            Console.WriteLine("WELCOME TO PROMEDICS");
+            Console.WriteLine("_DESCRIPTION_");
+
+            Console.WriteLine();
+
+            Console.WriteLine("PLEASE ENTER YOUR AGE:");
+            int User_Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("PLEASE ENTER YOUR SEX AT BIRTH:");
+            string User_Sex = Console.ReadLine();
+
+            // Display Benefit Programs
+
+            Console.WriteLine();
+
+            string Run = "Yes";
+            while (Run == "Yes") 
+                {
+                    Console.WriteLine("BENEFIT PROGRAMS");
+                    Console.WriteLine(@"
+                        1 - Outpatient Benefits  
+                        2 - Senior Citizen Benefits 
+                        3 - Inpatient Benefits 
+                        4 - Z Benefits
+                        5 - Maternity Benefits  
+                        6 - HELP");
+                    string BP_Choice = Console.ReadLine();
+                    if (BP_Choice == "1")
+                        {
+                            /* Display Summarized Outpatient Benefit Programs
+                            Input Selected Program
+                            Display Health Facilities with Available Selected Programs
+                            Display Selected Health Facility’s Location */
+                        }
+                    else if (BP_Choice == "2" && User_Age >= 60)
+                        { 
+                            /* Display Summarized Outpatient Benefit Programs
+                            Input Selected Program
+                            Display Health Facilities with Available Selected Programs
+                            Display Selected Health Facility’s Location */
+                        }
+                    else if (BP_Choice == "3")
+                        { 
+                            /* Display Summarized Outpatient Benefit Programs
+                            Input Selected Program
+                            Display Health Facilities with Available Selected Programs
+                            Display Selected Health Facility’s Location */
+                        }
+                    else if (BP_Choice == "4")
+                        { 
+                            /* Display Summarized Outpatient Benefit Programs
+                            Input Selected Program
+                            Display Health Facilities with Available Selected Programs
+                            Display Selected Health Facility’s Location */
+                        }
+                    else if (BP_Choice == "5")
+                        { 
+                            /* Display Summarized Outpatient Benefit Programs
+                            Input Selected Program
+                            Display Health Facilities with Available Selected Programs
+                            Display Selected Health Facility’s Location */
+                        }
+                    else if (BP_Choice == "6")
+                        { 
+                            /* Display Summarized Outpatient Benefit Programs
+                            Input Selected Program
+                            Display Health Facilities with Available Selected Programs
+                            Display Selected Health Facility’s Location */
+                        }
+                    else 
+                        { 
+                    Console.WriteLine("ERROR");
+                    Console.WriteLine("INVALID INPUT");
+                    Console.WriteLine();
+                    Console.WriteLine("RETURN TO SELECTION PAGE?");
+                    Run = Console.ReadLine();
+                        }
+            }
+            
+
             /* START
                 Display Welcome Prompt
                 Input Age
